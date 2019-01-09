805a1c6a4ca8598eee3763db359d8d16d404f3c2
Sriram <sriramb2000@gmail.com>
2019-01-09 13:47:06 -0800

    Header files done

---
+++ prj1/Arena.h
@@ -0,0 +1,42 @@
+// Arena.h
+///////////////////////////////////////////////////////////////////////////
+// Type definitions
+///////////////////////////////////////////////////////////////////////////
+
+#ifndef ARENA_H
+#define ARENA_H
+#include "globals.h"
+
+class Zombie;
+class Player;
+
+class Arena
+{
+  public:
+        // Constructor/destructor
+    Arena(int nRows, int nCols);
+    ~Arena();
+
+        // Accessors
+    int     rows() const;
+    int     cols() const;
+    Player* player() const;
+    int     zombieCount() const;
+    int     numZombiesAt(int r, int c) const;
+    bool    determineNewPosition(int& r, int& c, int dir) const;
+    void    display() const;
+
+        // Mutators
+    bool   addZombie(int r, int c);
+    bool   addPlayer(int r, int c);
+    bool   attackZombieAt(int r, int c, int dir);
+    bool   moveZombies();
+
+  private:
+    int     m_rows;
+    int     m_cols;
+    Player* m_player;
+    Zombie* m_zombies[MAXZOMBIES];
+    int     m_nZombies;
+};
+#endif
